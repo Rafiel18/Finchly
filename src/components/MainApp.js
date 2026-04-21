2b2516859e0ba0a398db3375b1a6d00b894172b8
Rafiel18 <rafaelroliveira37@gmail.com>
2026-04-21 01:51:11 -0300

    Atualizar o MainApp.js

--- src/components/MainApp.js
+++ src/components/MainApp.js
@@ -61,7 +61,9 @@ export default function MainApp({
       {tab === "expenses" && <Expenses d={d} save={save} />}
       {tab === "debts" && <Debts d={d} save={save} />}
       {tab === "invest" && <Investments d={d} save={save} />}
-      {tab === "simulation" && <Simulation d={d} salary={salary} remDays={remDays} />}
+      {tab === "simulation" && (
+  <Simulation d={d} salary={salary} remDays={remDays} />
+)}
 
       {tab === "settings" && (
         <Settings
